b5127c2e5110da97473d420363997ce87bb7c8db
Emmanuel Oh <emmaneugene@outlook.com>
2021-10-07 13:22:54 +0800

    Add xUnit testing framework

---
+++ CS301-Spend-Transactions-Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CS301_Spend_Transactions_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
