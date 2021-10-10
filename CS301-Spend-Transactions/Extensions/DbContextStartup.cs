83ef265d541e47f33f18dc058981566e627f2d19
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-10 13:08:20 +0800

    Extension: DbContextStartup: Remove MySQL.Data.EntityFrameworkCore.Extensions import
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Extensions/DbContextStartup.cs
+++ CS301-Spend-Transactions/Extensions/DbContextStartup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Microsoft.EntityFrameworkCore;
-using MySQL.Data.EntityFrameworkCore.Extensions;
 
 namespace CS301_Spend_Transactions.Extensions
 {
