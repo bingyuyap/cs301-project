59e15f9088d9d2e00de8c300bfd273437e3d801d
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-10 20:44:58 +0800

    Models: Transaction: Comment CardId and MerchantName
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Models/Transaction.cs
+++ CS301-Spend-Transactions/Models/Transaction.cs
@@ -6,7 +6,7 @@ namespace CS301_Spend_Transactions.Models
     public class Transaction
     {
         public string Id { get; set; }
-        public string CardId { get; set; } // references card table
+        // public string CardId { get; set; } // references card table
         public string MerchantName{ get; set; } // references merchant table
         public DateTime TransactionDate { get; set; }
         public string Currency { get; set; }
