c717337ab772b09b46d9a4ecc4812337de465566
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-10 12:04:50 +0800

    CS301-Spend-Transactions: Program: Add Serilog to log app name and environment name
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Program.cs
+++ CS301-Spend-Transactions/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace CS301_Spend_Transactions
 {
@@ -16,11 +19,29 @@ namespace CS301_Spend_Transactions
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Add Serilog for better loggin diagnostics
+        // For more reference: https://nblumhardt.com/2019/10/serilog-in-aspnetcore-3/ 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseSerilog((hostingContext, loggerConfiguration) => {
+                            loggerConfiguration
+                                .ReadFrom.Configuration(hostingContext.Configuration)
+                                .Enrich.FromLogContext()
+                                // Just to show the app name
+                                .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                                // To show environment running in
+                                .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment)
+                                .WriteTo.Console(theme: AnsiConsoleTheme.Literate);
+#if DEBUG
+                            // Used to filter out potentially bad data due debugging.
+                            // Very useful when doing Seq dashboards and want to remove logs under debugging session.
+                            loggerConfiguration.Enrich.WithProperty("DebuggerAttached", Debugger.IsAttached);
+#endif
+                        });
                 });
     }
 }
