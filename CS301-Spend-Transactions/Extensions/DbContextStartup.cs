b2a9555b9b61e091510e04097e5fbcb3d4553b1e
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-10 12:34:07 +0800

    Add method to get connection strings based on machine name
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Extensions/DbContextStartup.cs
+++ CS301-Spend-Transactions/Extensions/DbContextStartup.cs
@@ -1,7 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
 namespace CS301_Spend_Transactions.Extensions
 {
     public class DbContextStartup
     {
-        
+        private static string GetConnectionStrings(IWebHostEnvironment env, IConfiguration configuration,
+            IDictionary<string, object> vault)
+        {
+            if (!env.IsProduction())
+                return configuration.GetConnectionString($"DefaultConnection:{Environment.MachineName}");
+
+            // TODO: Need to set Db name during deployment
+            var connectionString = Environment.GetEnvironmentVariable("transactionDb");
+            return !string.IsNullOrEmpty(connectionString) ? connectionString : null;
+        }
     }
 }
