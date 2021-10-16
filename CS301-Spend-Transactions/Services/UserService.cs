fea685099d93e0d395c9cf32908e99602c20cb7f
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-17 05:34:40 +0800

    Service: UserService: DI logger and serviceScope and add constructor for UserService
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Services/UserService.cs
+++ CS301-Spend-Transactions/Services/UserService.cs
@@ -1,10 +1,23 @@
+using System.Data;
 using CS301_Spend_Transactions.Models;
 using CS301_Spend_Transactions.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CS301_Spend_Transactions.Services
 {
     public class UserService : IUserService
     {
+        private readonly ILogger<UserService> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public UserService(IServiceScopeFactory scopeFactory,
+            ILogger<UserService> logger)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+        }
+        
         public User getUserById(string Id)
         {
             throw new System.NotImplementedException();
@@ -15,4 +28,5 @@ namespace CS301_Spend_Transactions.Services
             throw new System.NotImplementedException();
         }
     }
+
 }
