6dfeb9029a29f5d354268c6bfa03e39f999f06c6
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-17 05:11:55 +0800

    Services: UserService: Extend from IUserService and implement the two methods added
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Services/UserService.cs
+++ CS301-Spend-Transactions/Services/UserService.cs
@@ -1,7 +1,18 @@
+using CS301_Spend_Transactions.Models;
+using CS301_Spend_Transactions.Services.Interfaces;
+
 namespace CS301_Spend_Transactions.Services
 {
-    public class UserService
+    public class UserService : IUserService
     {
-        
+        public User getUserById(string Id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User addUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
