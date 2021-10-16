7b826544840f435fed2e8f08365655a6ae165b5e
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-17 05:10:28 +0800

    Services: Interfaces: IUserService: Add getUserById method and addUser
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Services/Interfaces/IUserService.cs
+++ CS301-Spend-Transactions/Services/Interfaces/IUserService.cs
@@ -1,7 +1,11 @@
+using CS301_Spend_Transactions.Models;
+
 namespace CS301_Spend_Transactions.Services.Interfaces
 {
     public interface IUserService
     {
-        
+        User getUserById(string Id);
+
+        User addUser(User user);
     }
 }
