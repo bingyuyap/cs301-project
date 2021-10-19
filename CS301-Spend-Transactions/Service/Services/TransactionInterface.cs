ab30b849745209cfc5be25323e470374d3e2e817
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-19 18:26:59 +0800

    Add TransactionService that implements ITransactionService
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ CS301-Spend-Transactions/Service/Services/TransactionInterface.cs
@@ -0,0 +1,10 @@
+using CS301_Spend_Transactions.Controllers.Interfaces;
+using CS301_Spend_Transactions.Services.Interfaces;
+
+namespace CS301_Spend_Transactions.Services
+{
+    public class TransactionInterface : ITransactionService
+    {
+        
+    }
+}
