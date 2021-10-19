c37247a467ba99a38557f7a6ab014af0e259acbe
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-19 18:35:26 +0800

    Service: Services: TransactionService: Add Logger and Service Scope
    Service: Services: TransactionService: Inversion of Control DI
    
    Service: Services: TransactionService: Rename class
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/TransactionService.cs
+++ CS301-Spend-Transactions/Service/Services/TransactionService.cs
@@ -1,14 +1,31 @@
 using System.Transactions;
-using CS301_Spend_Transactions.Controllers.Interfaces;
 using CS301_Spend_Transactions.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CS301_Spend_Transactions.Services
 {
-    public class TransactionInterface : ITransactionService
+    public class TransactionService : ITransactionService
     {
+        private readonly ILogger<UserService> _logger;
+        // Manages the lifetime of the services we going to inject
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public TransactionService(IServiceScopeFactory scopeFactory,
+            ILogger<UserService> logger)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+        }
         public Transaction AddTransaction(Transaction transaction)
         {
-            throw new System.NotImplementedException();
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            dbContext.Add(transaction);
+            dbContext.SaveChanges();
+
+            return transaction;
         }
 
         public Transaction GetTransactionById(string Id)
