b96f4f0ba3be3e4ea24819c8e10275de85795426
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-19 18:27:50 +0800

    Service: Services: Interfaces: ITransactionService: Add AddTransaction and GetTransactionById
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/Interfaces/ITransactionService.cs
+++ CS301-Spend-Transactions/Service/Services/Interfaces/ITransactionService.cs
@@ -1,7 +1,11 @@
+using System.Transactions;
+
 namespace CS301_Spend_Transactions.Services.Interfaces
 {
     public interface ITransactionService
     {
-        
+        Transaction AddTransaction(Transaction transaction);
+
+        Transaction GetTransactionById(string Id);
     }
 }
