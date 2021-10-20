65d82d7b9e11fb16a31d1cb72f575fb5d8c455fe
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-20 10:32:33 +0800

    Repo: Helpers: DatabaseSeeder: Add logger and scopeFactory
    Repo: Helpers: DatabaseSeeder: IOC DI
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/DatabaseSeeder.cs
+++ CS301-Spend-Transactions/Repo/Helpers/DatabaseSeeder.cs
@@ -1,7 +1,19 @@
+using CS301_Spend_Transactions.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 namespace CS301_Spend_Transactions.Repo.Helpers
 {
     public class DatabaseSeeder
     {
-        
+        private readonly ILogger<UserService> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public DatabaseSeeder(IServiceScopeFactory scopeFactory,
+            ILogger<UserService> logger)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+        }
     }
 }
