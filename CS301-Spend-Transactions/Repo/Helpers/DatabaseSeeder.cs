6e515de392d9fa513d19bb69902a5cd1dc346dc8
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-20 11:28:02 +0800

    Repo: Helpers: DatabaseSeeder: Add seeding user logic
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/DatabaseSeeder.cs
+++ CS301-Spend-Transactions/Repo/Helpers/DatabaseSeeder.cs
@@ -1,4 +1,12 @@
+using System.IO;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CS301_Spend_Transactions.Models;
 using CS301_Spend_Transactions.Services;
+using CsvHelper;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,6 +22,28 @@ namespace CS301_Spend_Transactions.Repo.Helpers
         {
             _scopeFactory = scopeFactory;
             _logger = logger;
+        }
+
+        public void SeedUserEntries()
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "CS301-Spend-Transaction.Repo.Helpers.Seeds.dummy_users.csv";
+            
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader, 
+                        System.Globalization.CultureInfo.CreateSpecificCulture("enUS"));
+                    var users = csvReader.GetRecords<User>();
+                    
+                    dbContext.Users.AddRange(users);
+                    dbContext.SaveChanges();
+                }
+            }
         }
     }
 }
