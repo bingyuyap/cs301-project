f2c9d3a97e5e1086138664ee949239217a5367bb
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-20 14:04:56 +0800

    Controllers: UserController: Add seedUser end point
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Controllers/UserController.cs
+++ CS301-Spend-Transactions/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 using CS301_Spend_Transactions.Controllers.Interfaces;
 using CS301_Spend_Transactions.Models;
+using CS301_Spend_Transactions.Repo.Helpers;
+using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
 using CS301_Spend_Transactions.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -10,11 +12,15 @@ namespace CS301_Spend_Transactions.Controllers
     {
         private readonly ILogger<UserController> _logger;
         private IUserService _userService;
+        private IDatabaseSeeder _databaseSeeder;
 
-        public UserController(ILogger<UserController> logger, IUserService userService) : base(logger)
+        public UserController(ILogger<UserController> logger, 
+            IUserService userService,
+            IDatabaseSeeder databaseSeeder) : base(logger)
         {
             _logger = logger;
             _userService = userService;
+            _databaseSeeder = databaseSeeder;
         }
         
         [HttpPost("/api/User/AddUser")]
@@ -22,12 +28,19 @@ namespace CS301_Spend_Transactions.Controllers
         {
             return _userService.AddUser(user);
         }
-        
-        [HttpGet("/api/User/GetUser")]
 
-        public User GetUsetById(string Id)
+
+
+        [HttpGet("/api/User/GetUser")]
+        public User GetUserById(string Id)
         {
             return _userService.GetUserById(Id);
+        }
+
+        [HttpGet("/api/User/Seed")]
+        public async void SeedUsers()
+        {
+            var res = await _databaseSeeder.SeedUserEntries();
         }
     }
 }
