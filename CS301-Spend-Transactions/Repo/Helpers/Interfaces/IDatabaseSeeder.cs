5bad2de3818e8357a0cd87c9e8061c34318784f8
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-21 11:23:18 +0800

    Repo: Helpers: Interfaces: IDatabaseSeeder: Add SeedTransactionEntries method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/Interfaces/IDatabaseSeeder.cs
+++ CS301-Spend-Transactions/Repo/Helpers/Interfaces/IDatabaseSeeder.cs
@@ -7,5 +7,7 @@ namespace CS301_Spend_Transactions.Repo.Helpers.Interfaces
         Task<int> SeedUserEntries();
 
         Task<int> SeedCardEntries();
+        
+        Task<int> SeedTransactionEntries();
     }
 }
