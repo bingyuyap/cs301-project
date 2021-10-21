14e0b347ed63c52e4370bb53df9fd6d5e216e8f6
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-21 11:10:58 +0800

    Controllers: DatabaseControllers: Add logger and DatabaseSeeder helper and do IOC DI
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Controllers/DatabaseController.cs
+++ CS301-Spend-Transactions/Controllers/DatabaseController.cs
@@ -1,12 +1,18 @@
 using CS301_Spend_Transactions.Controllers.Interfaces;
+using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
 using Microsoft.Extensions.Logging;
 
 namespace CS301_Spend_Transactions.Controllers
 {
     public class DatabaseController : BaseController<DatabaseController>, IDatabaseController
     {
-        public DatabaseController(ILogger<DatabaseController> logger) : base(logger)
+        private readonly ILogger<DatabaseController> _logger;
+        private IDatabaseSeeder _databaseSeeder;
+        
+        public DatabaseController(ILogger<DatabaseController> logger, IDatabaseSeeder databaseSeeder) : base(logger)
         {
+            _logger = logger;
+            _databaseSeeder = databaseSeeder;
         }
 
         public void SeedUsers()
