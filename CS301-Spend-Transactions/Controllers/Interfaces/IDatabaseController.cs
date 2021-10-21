0d5acc8209d3095b5ff57f13c592a68974c02559
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-21 11:05:39 +0800

    Controllers: Interfaces: IDatabaseController: Add seeding methods
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Controllers/Interfaces/IDatabaseController.cs
+++ CS301-Spend-Transactions/Controllers/Interfaces/IDatabaseController.cs
@@ -2,6 +2,10 @@ namespace CS301_Spend_Transactions.Controllers.Interfaces
 {
     public interface IDatabaseController
     {
-        
+        void SeedUsers();
+
+        void SeedCards();
+
+        void SeedTransactions();
     }
 }
