15705bb22250100eaa2e981df8723fc9b3e19ead
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-22 14:27:14 +0800

    Domain: Entities: Group: Add attributes to group
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Domain/Entities/Groups.cs
+++ CS301-Spend-Transactions/Domain/Entities/Groups.cs
@@ -2,6 +2,10 @@ namespace CS301_Spend_Transactions.Models
 {
     public class Groups
     {
+        public int MinMCC { get; set; }
         
+        public int MaxMCC { get; set; }
+        
+        public string Name { get; set; }
     }
 }
