1df03622ce1db31b6cf483048e9dd183240d651a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-22 15:05:46 +0800

    Repo: Helpers: GroupMappingHelper: Add logic to find group
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/GroupMappingHelper.cs
+++ CS301-Spend-Transactions/Repo/Helpers/GroupMappingHelper.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using CS301_Spend_Transactions.Models;
 using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
 using CS301_Spend_Transactions.Services;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,7 +23,10 @@ namespace CS301_Spend_Transactions.Repo.Helpers
         // Won't affect the performance since the number of groups are limited
         public string GetGroupFromMCC(int MCC)
         {
-            
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            return dbContext.Groups.First(groups => groups.MinMCC < MCC && groups.MaxMCC > MCC).Name;
         }
     }
 }
