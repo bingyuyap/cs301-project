15c58adc1ff49609a4f03dcf02fcde3801542368
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-22 14:36:27 +0800

    Repo: Data: Migrations: Run dotnet ef migrations add and dotnet ef database update
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

---
+++ CS301-Spend-Transactions/Repo/Data/Migrations/20211022063449_AddGroups.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace CS301_Spend_Transactions.Migrations
+{
+    public partial class AddGroups : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "groups",
+                columns: table => new
+                {
+                    min_mcc = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    max_mcc = table.Column<int>(nullable: false),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("groups_pkey", x => x.min_mcc);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "groups");
+        }
+    }
+}
