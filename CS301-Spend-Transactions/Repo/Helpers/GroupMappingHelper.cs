393a8bb454a28e58a24a038f25e211a6f9e30907
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-22 14:52:26 +0800

    Repo: Helpers: GroupMappingHelper: Add logger and scopeFactory
    Repo: Helpers: GroupMappingHelper: IOC DI
    
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/GroupMappingHelper.cs
+++ CS301-Spend-Transactions/Repo/Helpers/GroupMappingHelper.cs
@@ -1,12 +1,25 @@
 using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
+using CS301_Spend_Transactions.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CS301_Spend_Transactions.Repo.Helpers
 {
     public class GroupMappingHelper : IGroupMappingHelper
     {
+        private readonly ILogger<UserService> _logger;
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public GroupMappingHelper(IServiceScopeFactory scopeFactory,
+            ILogger<UserService> logger)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+        }
+        
         public string GetGroupFromMCC(int MCC)
         {
-            throw new System.NotImplementedException();
+            
         }
     }
 }
