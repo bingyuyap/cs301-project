43c1e704a673fd8463bf3efee818e65cfa3aa8da
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-25 02:26:45 +0800

    Service: Services: CardService: Add GetCardById methdo
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/CardService.cs
+++ CS301-Spend-Transactions/Service/Services/CardService.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using CS301_Spend_Transactions.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,6 +16,15 @@ namespace CS301_Spend_Transactions.Services
         {
             _scopeFactory = scopeFactory;
             _logger = logger;
-        } 
+        }
+
+        public Card GetCardById(string Id)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            // Using LINQ expressions here
+            return dbContext.Cards.First(card => card.Id == Id);
+        }
     }
 }
