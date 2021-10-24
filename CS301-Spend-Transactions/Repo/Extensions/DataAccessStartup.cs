c50cdba67b13f5efb4f04adef00298e82a38c88a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-25 02:32:37 +0800

    Repo: Extensions: DataAccessStartup: Add services
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Extensions/DataAccessStartup.cs
+++ CS301-Spend-Transactions/Repo/Extensions/DataAccessStartup.cs
@@ -3,6 +3,7 @@ using CS301_Spend_Transactions.Repo.Helpers;
 using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
 using CS301_Spend_Transactions.Services;
 using CS301_Spend_Transactions.Services.Interfaces;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CS301_Spend_Transactions.Extensions
@@ -12,8 +13,11 @@ namespace CS301_Spend_Transactions.Extensions
         public static void AddDataAccessInjections(this IServiceCollection services)
         {
             // Service Injection
-            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<ICardService, CardService>();
+            services.AddTransient<IRuleService, RuleService>();
             services.AddTransient<ITransactionService, TransactionService>();
+            services.AddTransient<IUserService, UserService>();
+            
             services.AddTransient<IDatabaseSeeder, DatabaseSeeder>();
 
         }
