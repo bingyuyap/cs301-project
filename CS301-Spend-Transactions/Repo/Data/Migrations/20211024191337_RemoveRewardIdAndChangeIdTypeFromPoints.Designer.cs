d00b5702f8520cc88157f7dc79ec0797505f50fa
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-25 03:32:18 +0800

    Repo: Data: Migrations: Run migrations after removing reward FK
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Data/Migrations/20211024191337_RemoveRewardIdAndChangeIdTypeFromPoints.Designer.cs
+++ CS301-Spend-Transactions/Repo/Data/Migrations/20211024191337_RemoveRewardIdAndChangeIdTypeFromPoints.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CS301_Spend_Transactions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CS301_Spend_Transactions.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211024191337_RemoveRewardIdAndChangeIdTypeFromPoints")]
+    partial class RemoveRewardIdAndChangeIdTypeFromPoints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
