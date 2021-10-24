ccb9b4138594d9bea636881315302f0366d4a01a
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-24 22:33:40 +0800

    Service: Services: RewardService: Add dependencies and IOC DI
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/RewardService.cs
+++ CS301-Spend-Transactions/Service/Services/RewardService.cs
@@ -1,7 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 namespace CS301_Spend_Transactions.Services
 {
     public class RewardService
     {
+        private readonly ILogger<UserService> _logger;
+        // Manages the lifetime of the services we going to inject
+        private readonly IServiceScopeFactory _scopeFactory;
         
+        public RewardService(IServiceScopeFactory scopeFactory,
+            ILogger<UserService> logger)
+        {
+            _scopeFactory = scopeFactory;
+            _logger = logger;
+        }
     }
 }
