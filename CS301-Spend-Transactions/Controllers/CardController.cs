0ee9e297863a00996b7e66e1ba6f8026e1fbd1d5
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-25 03:36:06 +0800

    Controllers: CardController: Add dependencies and IOC DI
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Controllers/CardController.cs
+++ CS301-Spend-Transactions/Controllers/CardController.cs
@@ -1,7 +1,22 @@
+using CS301_Spend_Transactions.Controllers.Interfaces;
+using CS301_Spend_Transactions.Models;
+using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
+using CS301_Spend_Transactions.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace CS301_Spend_Transactions.Controllers
 {
-    public class CardController
+    public class CardController : BaseController<CardController>, ICardController
     {
-        
+        private readonly ILogger<CardController> _logger;
+        private ICardService _cardService;
+
+        public CardController(ILogger<CardController> logger, 
+            ICardService cardService) : base(logger)
+        {
+            _logger = logger;
+            _cardService = cardService;
+        }
     }
 }
