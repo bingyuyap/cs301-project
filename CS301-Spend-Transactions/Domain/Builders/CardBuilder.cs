d4e71a69f83cdd22d1a0b7ba5634b4b993c65c4b
Bing Yu <bingyu.yap.21@gmail.com>
2021-10-24 21:35:31 +0800

    Domain: Builders: CardBuilder: Implement Create method to encapsulate logic of creating a card
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Domain/Builders/CardBuilder.cs
+++ CS301-Spend-Transactions/Domain/Builders/CardBuilder.cs
@@ -1,7 +1,26 @@
+using System.Reflection.Metadata.Ecma335;
+using CS301_Spend_Transactions.Models;
+
 namespace CS301_Spend_Transactions.Domain.Builders
 {
     public class CardBuilder
     {
-        
+        private Card _card;
+
+        public CardBuilder Create(string Id, string UserId, string CardPan, string CardType)
+        {
+            if (CardType == "scis_shopping")
+                _card = new PointCard();
+            else if (CardType == "scis_platinummiles" || CardType == "scis_premiummiles")
+                _card = new MilesCard();
+            else if (CardType == "scis_freedom")
+                _card = new CashbackCard();
+            
+            _card.Id = Id;
+            _card.UserId = UserId;
+            _card.CardPan = CardPan;
+
+            return this;
+        }
     }
 }
