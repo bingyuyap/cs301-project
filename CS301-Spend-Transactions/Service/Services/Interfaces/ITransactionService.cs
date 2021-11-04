c2f6b1f3c9ed90df09ff3ab0946630e1718ca939
Emmanuel Oh <emmaneugene@outlook.com>
2021-11-04 18:36:59 +0800

    Services: Update TransactionService.AddTransaction method
    - Method now takes in TransactionDTO object as argument
    - Update ITransactionController, ITransactionService, TransactionController definitions
    - Create InvalidTransactionException

--- CS301-Spend-Transactions/Service/Services/Interfaces/ITransactionService.cs
+++ CS301-Spend-Transactions/Service/Services/Interfaces/ITransactionService.cs
@@ -1,12 +1,13 @@
 
+using CS301_Spend_Transactions.Domain.DTO;
 using CS301_Spend_Transactions.Models;
 
 namespace CS301_Spend_Transactions.Services.Interfaces
 {
     public interface ITransactionService
     {
-        Transaction AddTransaction(Transaction transaction);
+        Transaction AddTransaction(TransactionDTO transactionDto);
 
-        Transaction GetTransactionById(string Id);
+        Transaction GetTransactionById(string id);
     }
 }
--- CS301-Spend-Transactions/Service/Services/TransactionService.cs
+++ CS301-Spend-Transactions/Service/Services/TransactionService.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using CS301_Spend_Transactions.Domain.DTO;
+using CS301_Spend_Transactions.Domain.Exceptions;
 using CS301_Spend_Transactions.Models;
+using CS301_Spend_Transactions.Repo.Helpers;
 using CS301_Spend_Transactions.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,24 +24,76 @@ namespace CS301_Spend_Transactions.Services
             _scopeFactory = scopeFactory;
             _logger = logger;
         }
-        public Transaction AddTransaction(Transaction transaction)
+        
+        /**
+         * Adds a transaction into the database, handling all the necessary checks
+         */
+        public Transaction AddTransaction(TransactionDTO transactionDto)
         {
             using var scope = _scopeFactory.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-            dbContext.Add(transaction);
-            dbContext.SaveChanges();
+            var transaction = TransactionMapperHelper.ToTransaction(transactionDto);
+            
+            // 1-Validate transaction
+            if (transaction.Amount < 0)
+            {
+                throw new InvalidTransactionException("Transaction cannot have a negative amount");
+            }
+            
+            var card = dbContext.Cards.Find(transaction.CardId);
 
+            if (card is null)
+            {
+                throw new InvalidTransactionException("Invalid card ID in transaction record");
+            }
+            
+            // 2-Find any exclusions 
+            var mcc = int.Parse(transactionDto.MCC);
+            var exclusions = dbContext.Exclusions.Where(exclusion => exclusion.MCC == mcc);
+
+            if (exclusions.Any())
+            {
+                dbContext.Transactions.Add(transaction);
+                dbContext.SaveChanges();
+                return transaction;
+            }
+            
+            // 3-Check for all rules that apply, and create points
+            var foreignSpend = (!transactionDto.Currency.Equals("SGD"));
+            var rules = dbContext.Rules.Where(rule =>
+                rule.CardType == transactionDto.CardType
+                    && rule.MinSpend < transactionDto.Amount
+                    && rule.MaxSpend > transactionDto.Amount
+                    && rule.ForeignSpend == foreignSpend    
+            );
+
+            foreach (var rule in rules)
+            {
+                var points = new Points
+                {
+                    Amount = rule.GetReward(transaction.Amount),
+                    ProcessedDate = DateTime.Now,
+                    TransactionId = transaction.Id,
+                    PointsTypeId = rule.PointsTypeId
+                };
+
+                dbContext.Points.Add(points);
+            }
+            
+            // 4-Save changes to db
+            dbContext.Transactions.Add(transaction);
+            dbContext.SaveChanges();
             return transaction;
         }
 
-        public Transaction GetTransactionById(string Id)
+        public Transaction GetTransactionById(string id)
         {
             using var scope = _scopeFactory.CreateScope();
             var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
             // Using LINQ expressions here
-            return dbContext.Transactions.First(transaction => transaction.Id == Id);
+            return dbContext.Transactions.First(transaction => transaction.Id == id);
         }
     }
 }
