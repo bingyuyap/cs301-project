09bc347c9c7047ff49540e6dea0caf015a7ccdc5
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 11:18:09 +0800

    Service: HostedServices: TimedHostedService: Add ExecuteAsync and StopAsync
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
+++ CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
@@ -1,14 +1,38 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace CS301_Spend_Transactions.Service.HostedServices
 {
     public class TimedHostedService : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        
+        private readonly ILogger<TimedHostedService> _logger;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
         {
-            throw new System.NotImplementedException();
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Queue Injection Hosted Service is running");
+            await DoWork(stoppingToken);
+        }
+        
+        private async Task DoWork(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+                _logger.LogInformation(
+                    "Consume Scoped Service Hosted Service is working.");
+        }
+        
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Consume Scoped Service Hosted Service is stopping.");
+
+            await base.StopAsync(stoppingToken);
         }
     }
 }
