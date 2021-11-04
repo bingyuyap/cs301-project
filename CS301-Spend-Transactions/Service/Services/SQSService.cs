4e7fecfc1030a5c72b559f8cc87fcf137e73b6f0
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 12:50:12 +0800

    Service: Services: SQSService: Use interface SQSHelepr
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/SQSService.cs
+++ CS301-Spend-Transactions/Service/Services/SQSService.cs
@@ -3,17 +3,19 @@ using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using CS301_Spend_Transactions.Repo.Helpers;
+using CS301_Spend_Transactions.Repo.Helpers.Interfaces;
+using CS301_Spend_Transactions.Services.Interfaces;
 using Microsoft.Extensions.Logging;
 using Org.BouncyCastle.Asn1.BC;
 
 namespace CS301_Spend_Transactions.Services
 {
-    public class SQSService
+    public class SQSService : ISQSService
     {
         private readonly ILogger<SQSService> _logger;
-        private readonly SQSHelper _sqsHelper;
+        private readonly ISQSHelper _sqsHelper;
 
-        public SQSService(ILogger<SQSService> logger, SQSHelper sqsHelper)
+        public SQSService(ILogger<SQSService> logger, ISQSHelper sqsHelper)
         {
             _logger = logger;
             _sqsHelper = sqsHelper;
