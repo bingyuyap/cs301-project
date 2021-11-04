66a5c4f57cefbf993de9b5ac240e6e0af1ea89de
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 12:20:10 +0800

    Service: Services: SQSService: Add logic to log every message
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/SQSService.cs
+++ CS301-Spend-Transactions/Service/Services/SQSService.cs
@@ -1,19 +1,31 @@
 using System.Threading.Tasks;
 using Amazon.SQS;
 using Amazon.SQS.Model;
+using CS301_Spend_Transactions.Repo.Helpers;
 using Microsoft.Extensions.Logging;
+using Org.BouncyCastle.Asn1.BC;
 
 namespace CS301_Spend_Transactions.Services
 {
     public class SQSService
     {
         private readonly ILogger<SQSService> _logger;
-        private readonly IAmazonSQS _sqs;
+        private readonly SQSHelper _sqsHelper;
 
-        public SQSService(ILogger<SQSService> logger, IAmazonSQS sqs)
+        public SQSService(ILogger<SQSService> logger, SQSHelper sqsHelper)
         {
             _logger = logger;
-            _sqs = sqs;
+            _sqsHelper = sqsHelper;
+        }
+
+        public async void GetMessages()
+        {
+            var messages = await _sqsHelper.GetMessage();
+
+            foreach (var message in messages)
+            {
+                _logger.LogInformation(message.ToString());
+            }   
         }
     }
 }
