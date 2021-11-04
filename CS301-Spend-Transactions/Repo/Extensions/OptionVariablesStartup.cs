0129249f67d6055a309d5cbeed5fa7c6d9221ff9
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 11:59:03 +0800

    Repo: Extensions: OptionVariablesStartup: Add sqs options
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Extensions/OptionVariablesStartup.cs
+++ CS301-Spend-Transactions/Repo/Extensions/OptionVariablesStartup.cs
@@ -1,7 +1,38 @@
+using System;
+using System.IO;
+using CS301_Spend_Transactions.Domain.Configurations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace CS301_Spend_Transactions.Extensions
 {
-    public class OptionVariablesStartup
+    public static class OptionVariablesStartup
     {
-        
+        public static void AddOptionVariables(this IServiceCollection services, IWebHostEnvironment env,
+            IConfiguration configuration)
+        {
+            var sqsConfig =  GetSqsOption(configuration, env);
+            
+            if (sqsConfig == null)
+                throw new InvalidDataException("Invalid rabbitMqConfig object received.");
+            
+            services.Configure<SQSOption>(options =>
+            {
+                options.QueueURL = sqsConfig.QueueURL;
+                options.Region = sqsConfig.Region;
+            });
+        }
+
+        private static SQSOption GetSqsOption(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            var queueUrl = Environment.GetEnvironmentVariable("queueUrl");
+            var region = Environment.GetEnvironmentVariable("sqsRegion");
+
+            if (string.IsNullOrEmpty(queueUrl) || string.IsNullOrEmpty(region))
+                return null;
+
+            return new SQSOption(queueUrl, region);
+        }
     }
 }
