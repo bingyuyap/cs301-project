8e3cdc3df3a0de7e6ea92d3e71936e98a25744d9
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 12:07:38 +0800

    Repo: Helpers: SQSHelper: Add dependecnies
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/SQSHelper.cs
+++ CS301-Spend-Transactions/Repo/Helpers/SQSHelper.cs
@@ -1,7 +1,19 @@
+using Amazon.SQS;
+using CS301_Spend_Transactions.Domain.Configurations;
+using CS301_Spend_Transactions.Services;
+using Microsoft.Extensions.Options;
+
 namespace CS301_Spend_Transactions.Repo.Helpers
 {
     public class SQSHelper
     {
-        
+        private readonly IAmazonSQS _sqs;
+        private readonly SQSOption _option;
+
+        public SQSHelper(IAmazonSQS sqs, IOptions<SQSOption> option)
+        {
+            _sqs = sqs;
+            _option = option.Value;
+        }
     }
 }
