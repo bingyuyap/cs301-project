a2af7b89a5825aa8a5c9c8741d0a814ae45a4e9d
Emmanuel Oh <emmaneugene@outlook.com>
2021-11-04 15:07:22 +0800

    Revert "feat: Create services and classes to handle data from SQS"
    This reverts commit 2c557d3334e8fb6d3457a2c66a2bb1c42e3353f8.

--- CS301-Spend-Transactions/Service/Services/Interfaces/IUserEntriesService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CS301_Spend_Transactions.Models;
-
-namespace CS301_Spend_Transactions.Services.Interfaces
-{
-    public interface IUserEntriesService
-    {
-        bool InsertUserEntry(UserEntry userEntry);
-    }
-}
