56d703da9eb181f7499d65723530f722c2883a61
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 10:48:02 +0800

    Service: HostedServices: TimedHostedService: Inherit IHostedService
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
+++ CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
@@ -1,7 +1,19 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
 namespace CS301_Spend_Transactions.Service.HostedServices
 {
-    public class TimedHostedService
+    public class TimedHostedService : IHostedService
     {
-        
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
