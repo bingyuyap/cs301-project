f4143ecd79e971a4d1662de719065bd6177e5344
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 11:45:29 +0800

    Domain: Configuration: SQSConfiguration: Add configuration variables for SQS
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Domain/Configurations/SQSConfiguration.cs
+++ CS301-Spend-Transactions/Domain/Configurations/SQSConfiguration.cs
@@ -2,6 +2,15 @@ namespace CS301_Spend_Transactions.Domain.Configurations
 {
     public class SQSConfiguration
     {
-        
+        public SQSConfiguration(string queueUrl, string region)
+        {
+            QueueURL = queueUrl;
+            Region = region;
+        }
+
+        public string QueueURL { get; set; }
+
+        public string Region { get; set; }
+
     }
 }
