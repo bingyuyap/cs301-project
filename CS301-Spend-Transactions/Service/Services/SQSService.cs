7701c0a22d48ff8543d75dfce3163f39c02fac1e
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-04 11:40:21 +0800

    Service: Services: SQSService: Add dependencies
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/Services/SQSService.cs
+++ CS301-Spend-Transactions/Service/Services/SQSService.cs
@@ -1,7 +1,19 @@
+using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Logging;
+
 namespace CS301_Spend_Transactions.Services
 {
     public class SQSService
     {
-        
+        private readonly ILogger<SQSService> _logger;
+        private readonly IAmazonSQS _sqs;
+
+        public SQSService(ILogger<SQSService> logger, IAmazonSQS sqs)
+        {
+            _logger = logger;
+            _sqs = sqs;
+        }
     }
 }
