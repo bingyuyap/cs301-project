32a839c1470162e349cc772d2e451b2f4bbe5242
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-06 14:15:33 +0800

    Service: HostedServices: TimedHostedServices: Clean up code and comment to rationalize choices
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
+++ CS301-Spend-Transactions/Service/HostedServices/TimedHostedService.cs
@@ -50,32 +50,17 @@ namespace CS301_Spend_Transactions.Service.HostedServices
             {
                 var messages = _sqsService.GetMessages();
                 _logger.LogInformation($"Consumed {messages.Result.Count} messages from SQS");
-                // var message = _sqsService.GetSingleMessage();
-
-                // if (message is null) return;
 
                 var dtos = messages.Result.Select(m => { return TransactionMapperHelper.ToTransactionDTO(m.Body); });
                 _logger.LogInformation($"Converted {dtos.Count()} messages to DTO");
-                // var dto = TransactionMapperHelper.ToTransactionDTO(message.Result.Body);
 
-
-                // try
-                // {
-                //     _logger.LogInformation(dto.ToString());
-                //     _merchantService.AddMerchant(dto);
-                //     _transactionService.AddTransaction(dto);
-                // }
-                // catch (InvalidTransactionException e)
-                // {
-                //     _logger.LogCritical(
-                //         $"[TimedHostedService/DoWork] Transaction {dto.Transaction_Id} failed due to {e.Message}");
-                // }
-                
                 Parallel.ForEach(dtos, dto =>
                 {
                     try
                     {
                         _logger.LogInformation(dto.ToString());
+                        
+                        // cannot async because need merchant as fk to index transactions
                         _merchantService.AddMerchant(dto);
                         _transactionService.AddTransaction(dto);
                     }
@@ -85,17 +70,15 @@ namespace CS301_Spend_Transactions.Service.HostedServices
                             $"[TimedHostedService/DoWork] Transaction {dto.Transaction_Id} failed due to {e.Message}");
                     }
                 });
+                
             });
-           
-
-
-
             var checkpoint2 = sw.ElapsedMilliseconds;
-            
             _logger.LogInformation($"Time taken for parallel indexing {checkpoint2 - checkpoint1}");
+            
             _logger.LogInformation($"Time taken from consuming to indexing {sw.ElapsedMilliseconds}");
 
-            // await Task.Delay(5, stoppingToken);
+            // not delaying for now as we want to maximize performance
+            // await Task.Delay(1, stoppingToken);
         }
     }
 }
