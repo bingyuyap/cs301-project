cf6b758a2a0065e172762a97e31e3b82d6737ef0
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-06 09:10:09 +0800

    Controller: TransactionController: Invoke async method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Controllers/Interfaces/ITransactionController.cs
+++ CS301-Spend-Transactions/Controllers/Interfaces/ITransactionController.cs
@@ -1,4 +1,5 @@
 
+using System.Threading.Tasks;
 using CS301_Spend_Transactions.Domain.DTO;
 using CS301_Spend_Transactions.Models;
 
@@ -6,7 +7,7 @@ namespace CS301_Spend_Transactions.Controllers.Interfaces
 {
     public interface ITransactionController
     {
-        Transaction AddTransaction(TransactionDTO transactionDto);
+        Task<Transaction> AddTransaction(TransactionDTO transactionDto);
 
         Transaction GetTransactionById(string Id);
     }
