28f15f17d056f17534050a6bbe5194a33c3ee597
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-07 23:56:26 +0800

    MIgrations: Add failed-transactions table
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Migrations/20211107145003_AddFailedTransactions.Designer.cs
+++ CS301-Spend-Transactions/Migrations/20211107145003_AddFailedTransactions.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CS301_Spend_Transactions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CS301_Spend_Transactions.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211107145003_AddFailedTransactions")]
+    partial class AddFailedTransactions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
