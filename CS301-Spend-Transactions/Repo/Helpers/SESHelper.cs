66d60d43cd6255c3b9c36b976ea97b6e16e38cc6
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-07 18:40:05 +0800

    Repo: Helpers: SESHelper: Add sample code from AWS
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/SESHelper.cs
+++ CS301-Spend-Transactions/Repo/Helpers/SESHelper.cs
@@ -1,7 +1,93 @@
+using Amazon;
+using System;
+using System.Collections.Generic;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+
 namespace CS301_Spend_Transactions.Repo.Helpers
 {
     public class SESHelper
     {
-        
+        // Replace sender@example.com with your "From" address.
+        // This address must be verified with Amazon SES.
+        static readonly string senderAddress = "sender@example.com";
+
+        // Replace recipient@example.com with a "To" address. If your account
+        // is still in the sandbox, this address must be verified.
+        static readonly string receiverAddress = "recipient@example.com";
+
+        // The configuration set to use for this email. If you do not want to use a
+        // configuration set, comment out the following property and the
+        // ConfigurationSetName = configSet argument below. 
+        static readonly string configSet = "ConfigSet";
+
+        // The subject line for the email.
+        static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
+
+        // The email body for recipients with non-HTML email clients.
+        static readonly string textBody = "Amazon SES Test (.NET)\r\n"
+                                          + "This email was sent through Amazon SES "
+                                          + "using the AWS SDK for .NET.";
+
+        // The HTML body of the email.
+        string htmlBody = @"<html>
+        <head></head>
+        <body>
+          <h1>Amazon SES Test (AWS SDK for .NET)</h1>
+          <p>This email was sent with
+            <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+            <a href='https://aws.amazon.com/sdk-for-net/'>
+              AWS SDK for .NET</a>.</p>
+        </body>
+        </html>";
+
+        public async void SendFailedTransactionEmail(string transactionId)
+        {
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = senderAddress,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                            new List<string> { receiverAddress }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = htmlBody
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = textBody
+                            }
+                        }
+                    },
+                    // If you are not using a configuration set, comment
+                    // or remove the following line 
+                    ConfigurationSetName = configSet
+                };
+                try
+                {
+                    Console.WriteLine("Sending email using Amazon SES...");
+                    await client.SendEmailAsync(sendRequest);
+                    Console.WriteLine("The email was sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The email was not sent.");
+                    Console.WriteLine("Error message: " + ex.Message);
+
+                }
+            }
+
+        }
     }
 }
