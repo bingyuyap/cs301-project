30b48a9d0e7dd8a45a7f3cbaf5d23ebfe603fd2a
Bing Yu <bingyu.yap.21@gmail.com>
2021-11-07 18:26:09 +0800

    Repo: Helpers: FailedTransactionErrorHelper: Implement persistFailedTransaction and DtoToFailedTransaction method
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- CS301-Spend-Transactions/Repo/Helpers/FailedTransactionErrorHelper.cs
+++ CS301-Spend-Transactions/Repo/Helpers/FailedTransactionErrorHelper.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using System.Transactions;
 using CS301_Spend_Transactions.Domain.DTO;
+using CS301_Spend_Transactions.Models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -19,6 +22,50 @@ namespace CS301_Spend_Transactions.Repo.Helpers
 
         public void handleFailedTransaction(TransactionDTO transactionDto)
         {
+        }
+
+        private void persistFailedTransaction(TransactionDTO transactionDto)
+        {
+            using var scope = _scopeFactory.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            var failedTransactions = dbContext.FailedTransactions.Where(
+                t => t.Transaction_Id == transactionDto.Transaction_Id
+            );
+
+            FailedTransaction failedTransaction = null;
+
+            if (failedTransactions.Any())
+            {
+                failedTransaction = failedTransactions.First();
+            }
+            else
+            {
+                failedTransaction = DtoToFailedTransaction(transactionDto);
+            }
+
+            failedTransaction.Count++;
+
+            dbContext.FailedTransactions.Add(failedTransaction);
+            dbContext.SaveChangesAsync();
+        }
+
+        private FailedTransaction DtoToFailedTransaction(TransactionDTO transactionDto)
+        {
+            return new FailedTransaction
+            {
+                Id = transactionDto.Id,
+                Transaction_Id = transactionDto.Transaction_Id,
+                Merchant = transactionDto.Merchant,
+                MCC = transactionDto.MCC,
+                Currency = transactionDto.Currency,
+                Amount = transactionDto.Amount,
+                Transaction_Date = transactionDto.Transaction_Date,
+                Card_Id = transactionDto.Card_Id,
+                Card_Pan = transactionDto.Card_Pan,
+                Card_Type = transactionDto.Card_Type,
+                Count = 0
+            };
         }
     }
 }
